29bf0bf692f68765d62f487ef549582dd7ae33cb
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-31 23:44:40 -0500

    setting

---
+++ src/test/java/em/fire/backend/FireBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package em.fire.backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FireBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
