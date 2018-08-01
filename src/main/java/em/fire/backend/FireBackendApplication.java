0aea9e1afe740e9d4f231d1b57dcdafeeb1cc3ad
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-31 23:59:06 -0500

    fire controller

--- src/main/java/em/fire/backend/FireBackendApplication.java
+++ src/main/java/em/fire/backend/FireBackendApplication.java
@@ -2,9 +2,19 @@ package em.fire.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class FireBackendApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String fire() {
+		return "Fire Backend!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FireBackendApplication.class, args);
