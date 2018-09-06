dfccb2590d8c902172ef75cd8598afeb63ef41df
Edwin Mendez <emendez@gravitalsoftware.com>
2018-09-05 19:06:22 -0500

    CrossOrigin

--- src/main/java/em/fire/backend/controller/UserController.java
+++ src/main/java/em/fire/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ public class UserController {
 		return "Fire Backend!!";
 	}	
 	
+	@CrossOrigin(origins = "*")
 	@PostMapping("/user/save")
 	public User createUser(@Valid @RequestBody User user) {
 		return userService.createUser(user);	
